58554c2656f9fe7f567e6a71af26f545fe9754c8
enzo-beltrami <enzo.beltrami@assertivasolucoes.com.br>
2019-01-11 14:50:26 -0200

    some fixes

--- dao/lambdaDao.js
+++ dao/lambdaDao.js
@@ -7,5 +7,6 @@ exports.getTemplateById = async idEmpresa => {
 		.input('id_empresa', idEmpresa)
 		.query('SELECT * FROM empresa WHERE id_empresa=@id_empresa');
 	pool.close();
-	return result.recordSets;
+	console.log('Query result: ', result);
+	return result.recordsets[0];
 };
--- resources/dbConfig.js
+++ resources/dbConfig.js
@@ -1,7 +1,7 @@
 exports.config = {
 	user: process.env.user,
 	password: process.env.password,
-	server: process.env.server,
+	server: process.env.host,
 	database: process.env.database,
 	port: Number(process.env.port),
 };
